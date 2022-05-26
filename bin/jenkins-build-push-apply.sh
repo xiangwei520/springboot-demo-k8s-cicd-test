c0d46e97dbfae603f26790eff4444c75d25d663d
xiangwei520 <2458885337@qq.com>
2022-05-26 15:39:27 +0800

    @xiangwei@修改sh文件

--- bin/jenkins-build-push-apply.sh
+++ bin/jenkins-build-push-apply.sh
@@ -1,8 +1,5 @@
-ssh -f root@192.168.109.102 'kubectl delete -f /opt/springboot/springboot-demo.yaml'
-echo "kubectl delete springboot-demo.yaml ok!"
-sleep 5
-ssh -f root@192.168.109.102 'rm -rf /opt/springboot/*'
-
+ssh -f root@192.168.109.102 'kubectl delete -f /opt/springboot/springboot-demo.yaml ; echo "kubectl delete springboot-demo.yaml ok!" ; rm -rf /opt/springboot/*'
+sleep 10
 rsync /root/.jenkins/workspace/springboot-demo-k8s-cicd-test/target/springboot-demo-k8s-cicd-test-1.1.0-RELEASE.jar root@192.168.109.102:/opt/springboot/
 rsync /root/.jenkins/workspace/springboot-demo-k8s-cicd-test/Dockerfile root@192.168.109.102:/opt/springboot/
 rsync /root/.jenkins/workspace/springboot-demo-k8s-cicd-test/bin/springboot-demo.yaml root@192.168.109.102:/opt/springboot/
