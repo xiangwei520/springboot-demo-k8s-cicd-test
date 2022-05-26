b4c328aec29ad8a03aac0d36d865040ba80128cb
xiangwei520 <2458885337@qq.com>
2022-05-26 13:57:25 +0800

    @xiangwei@jenkins-build-push-apply

---
+++ bin/jenkins-build-push-apply.sh
@@ -0,0 +1,7 @@
+ssh -f root@192.168.109.102 'rm -rf /opt/springboot/*'
+
+rsync /root/.jenkins/workspace/springboot-demo-k8s-cicd-test/target/springboot-demo-k8s-cicd-test-1.0.5-SNAPSHOT.jar root@192.168.109.102:/opt/springboot/
+rsync /root/.jenkins/workspace/springboot-demo-k8s-cicd-test/Dockerfile root@192.168.109.102:/opt/springboot/
+rsync /root/.jenkins/workspace/springboot-demo-k8s-cicd-test/bin/springboot-demo.yaml root@192.168.109.102:/opt/springboot/
+
+ssh root@192.168.109.102 'docker rmi $(docker images | grep "springboot-demo-k8s-cicd-test" | awk '{print $3}') ; echo "docker rmi old images ok!" ; docker build -t registry.cn-hangzhou.aliyuncs.com/xiangwei-public/springboot-demo-k8s-cicd-test:1.0.5-SNAPSHOT /opt/springboot/. ; echo "docker build ok!" ; docker push registry.cn-hangzhou.aliyuncs.com/xiangwei-public/springboot-demo-k8s-cicd-test:1.0.5-SNAPSHOT ; echo "docker push ok!"  ; docker rmi registry.cn-hangzhou.aliyuncs.com/xiangwei-public/springboot-demo-k8s-cicd-test:1.0.5-SNAPSHOT ; echo "docker rmi new images ok!" ; kubectl delete -f /opt/springboot/springboot-demo.yaml ; echo "kubectl delete springboot-demo.yaml ok!" ; kubectl apply -f /opt/springboot/springboot-demo.yaml ; echo "kubectl apply springboot-demo.yaml ok!" ; rm -rf /opt/springboot/springboot-demo-k8s-cicd-test-1.0.5-SNAPSHOT.jar ; rm -rf /opt/springboot/Dockerfile'
